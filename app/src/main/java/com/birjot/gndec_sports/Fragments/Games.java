f69e6df39e2ec49251e6087ebec2a82c8266a6a3
birjotsingh17 <birjotsingh17@gmail.com>
2017-11-18 00:09:03 +0530

    pull hi ni hoya shi tra,fr to krna paareA

--- app/src/main/java/com/birjot/gndec_sports/Fragments/Games.java
+++ app/src/main/java/com/birjot/gndec_sports/Fragments/Games.java
@@ -1,10 +1,9 @@
-package com.birjot.gndec_sports_admin.Fragments;
+package com.birjot.gndec_sports.Fragments;
 
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
